1145a832e9b19a79e690fcc881425399d9465d50
Wesley Aptekar-Cassels <W.Aptekar@gmail.com>
2017-01-04 05:04:57 -0800

    Move all shim functions into panic.rs

--- src/panic.rs
+++ src/panic.rs
@@ -1,9 +1,19 @@
 use core::fmt;
 
+#[lang = "eh_personality"]
+extern fn eh_personality() {
+}
+
 #[lang = "panic_fmt"]
 #[no_mangle]
 pub extern fn rust_begin_panic(_msg: fmt::Arguments,
                                _file: &'static str,
                                _line: u32) -> ! {
+    loop {}
+}
+
+#[allow(non_snake_case)]
+#[no_mangle]
+pub extern "C" fn _Unwind_Resume() -> ! {
     loop {}
 }
