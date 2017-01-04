cfbb5d2d785ce552f0a96368423dc6c8dc660339
Wesley Aptekar-Cassels <W.Aptekar@gmail.com>
2017-01-04 14:15:54 -0800

    Refactor allocator code

--- src/subatomic.rs
+++ src/subatomic.rs
@@ -18,52 +18,38 @@ mod panic;
 use memory::FrameAllocator;
 
 #[no_mangle]
-pub extern fn kmain(multiboot_information_address: usize) -> ! {
+pub extern fn kmain(multiboot_info_addr: usize) -> ! {
     console::set_color(console::color::ColorCode::new(console::color::Color::LightBlue, console::color::Color::Black));
     println!("Welcome to subatomic");
     console::set_color(console::color::ColorCode::new(console::color::Color::Green, console::color::Color::Black));
-    let boot_info = unsafe{ multiboot2::load(multiboot_information_address) };
+
+    let mut frame_allocator = setup_allocator(multiboot_info_addr);
+    let mut kbd: input::poll::PollingKeyboard = input::poll::PollingKeyboard::new(handle_char);
+
+    loop{
+        kbd.update();
+    }
+}
+
+fn setup_allocator(multiboot_info_addr: usize) -> memory::AreaFrameAllocator {
+    let boot_info = unsafe{ multiboot2::load(multiboot_info_addr) };
     let memory_map_tag = boot_info.memory_map_tag()
         .expect("Memory map tag required");
 
-    println!("memory areas:");
-    for area in memory_map_tag.memory_areas() {
-        println!("    start: 0x{:x}, length: 0x{:x}",
-            area.base_addr, area.length);
-    }
-
     let elf_sections_tag = boot_info.elf_sections_tag()
         .expect("Elf-sections tag required");
-
-    println!("kernel sections:");
-    for section in elf_sections_tag.sections() {
-        println!("    addr: 0x{:x}, size: 0x{:x}, flags: 0x{:x}",
-            section.addr, section.size, section.flags);
-    }
 
     let kernel_start = elf_sections_tag.sections().map(|s| s.addr)
         .min().unwrap();
     let kernel_end = elf_sections_tag.sections().map(|s| s.addr + s.size)
         .max().unwrap();
 
-    println!("Kernel:");
-    println!("Start: 0x{:x} End: 0x{:x}", kernel_start, kernel_end);
-
-    let multiboot_start = multiboot_information_address;
+    let multiboot_start = multiboot_info_addr;
     let multiboot_end = multiboot_start + (boot_info.total_size as usize);
 
-    println!("Multiboot:");
-    println!("Start: 0x{:x} End: 0x{:x}", multiboot_start, multiboot_end);
-
-    let mut frame_allocator = memory::AreaFrameAllocator::new(
+    memory::AreaFrameAllocator::new(
         kernel_start as usize, kernel_end as usize, multiboot_start,
-        multiboot_end, memory_map_tag.memory_areas());
-
-    let mut kbd: input::poll::PollingKeyboard = input::poll::PollingKeyboard::new(handle_char);
-
-    loop{
-        kbd.update();
-    }
+        multiboot_end, memory_map_tag.memory_areas())
 }
 
 fn handle_char(c: char) {
