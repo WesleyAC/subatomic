79ea114649862dfe89abc88f2b7a8fa8e4d6c457
Wesley Aptekar-Cassels <W.Aptekar@gmail.com>
2017-01-04 04:28:12 -0800

    Add VGA driver

--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,18 @@
 #![feature(lang_items)]
+#![feature(const_fn)]
+#![feature(unique)]
 #![no_std]
 
 extern crate rlibc;
+extern crate volatile;
+extern crate spin;
 
 pub mod panic;
+mod vga;
 
 #[no_mangle]
 pub extern fn kmain() -> ! {
-    let hello = b"Hello World!";
-    let color_byte = 0x1f; // white foreground, blue background
-
-    let mut hello_colored = [color_byte; 24];
-    for (i, char_byte) in hello.into_iter().enumerate() {
-        hello_colored[i*2] = *char_byte;
-    }
-
-    // write `Hello World!` to the center of the VGA text buffer
-    let buffer_ptr = (0xb8000 + 1988) as *mut _;
-    unsafe { *buffer_ptr = hello_colored };
-
+    vga::WRITER.lock().write_str("aaaaa");
     loop{}
 }
 
