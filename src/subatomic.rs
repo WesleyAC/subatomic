4e1c7bf23082f56fc0d89d0c2e17861894bb4dd0
Wesley Aptekar-Cassels <W.Aptekar@gmail.com>
2017-07-12 20:16:41 -0400

    Add more debugging info to memory allocator

--- src/subatomic.rs
+++ src/subatomic.rs
@@ -45,6 +45,12 @@ fn setup_allocator(multiboot_info_addr: usize) -> memory::AreaFrameAllocator {
     let elf_sections_tag = boot_info.elf_sections_tag()
         .expect("Elf-sections tag required");
 
+    println!("memory areas:");
+    for area in memory_map_tag.memory_areas() {
+        println!("start: 0x{:x}, length: 0x{:x}",
+            area.base_addr, area.length);
+    }
+
     println!("kernel sections:");
     for section in elf_sections_tag.sections() {
         println!("addr: 0x{:x}, size: 0x{:x}, flags: 0x{:x}",
